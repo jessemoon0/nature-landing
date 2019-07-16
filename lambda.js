86717bcff4169094beb1b1775f197fc531fe53d4
Jessie J Valladares <shajessie@me.com>
2019-07-16 11:49:13 -0400

    Testing mime type in api gateway

--- lambda.js
+++ lambda.js
@@ -20,9 +20,7 @@ const binaryMimeTypes = [
   'text/xml',
   'image/x-icon',
   'image/svg+xml',
-  'application/x-font-ttf',
-  'video/mp4',
-  'video/webm'
+  'application/x-font-ttf'
 ];
 
 server.app.use(awsServerlessExpressMiddleware.eventContext());
